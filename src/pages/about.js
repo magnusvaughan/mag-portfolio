a300052100c5ebb55b6810d8d50d3341118ec0db
mvaughan <magnus.vaughan@gmail.com>
2020-01-03 17:09:27 +0000

    Moved images to the src folder

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import c7 from "../../public/img/c7.png";
-import hbt from "../../public/img/hbt.png";
-import spl from "../../public/img/spl.png";
+import c7 from "../images/c7.png";
+import hbt from "../images/hbt.png";
+import spl from "../images/spl.png";
 
 function AboutPage() {
   return (
