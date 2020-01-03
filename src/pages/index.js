4c700d0c5437a5eea669c512034f26b155561d3a
mvaughan <magnus.vaughan@gmail.com>
2020-01-03 15:46:35 +0000

    Removed paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ function IndexPage() {
 
       <section className="text-center">
         <h2 className="font-display font-bold text-gray-600 text-3xl">I'm a software developer working with PHP and JavaScript</h2>
-        <p className="text-xl text-gray-600">I love building projects using Laravel and React</p>
       </section>
     </Layout>
   );
