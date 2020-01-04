18baa08a34e6b205055be75c6c7ca2e2e7aa1481
mvaughan <magnus.vaughan@gmail.com>
2020-01-04 21:22:45 +0000

    Responsive type sizes for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ function IndexPage() {
       />
 
       <section className="text-center">
-        <h2 className="font-display font-bold text-gray-800 text-6xl">I'm a software developer</h2>
-        <h3 className="font-display font-bold text-gray-800 text-4xl">working with PHP and JavaScript</h3>
+        <h2 className="font-display font-bold text-gray-800 text-3xl sm:text-4xl md:text-6xl">I'm a software developer</h2>
+        <h3 className="font-display font-bold text-gray-800 text-2xl sm:text-3xl md:text-5xl">working with PHP and JavaScript</h3>
       </section>
     </Layout>
   );
